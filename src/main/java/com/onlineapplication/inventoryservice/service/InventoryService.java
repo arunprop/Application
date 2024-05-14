970069069cde159da356dc6eea109d39e60bd42c
arunprop <valluruarunkumar@gmail.com>
2024-05-14 16:06:00 -0500

    update inventory to Main

--- src/main/java/com/onlineapplication/inventoryservice/service/InventoryService.java
+++ src/main/java/com/onlineapplication/inventoryservice/service/InventoryService.java
@@ -11,7 +11,7 @@ public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
-
+//jjfk
     public boolean isInStock(String skuCode){
          return inventoryRepository.findBySkuCode().isPresent();
 

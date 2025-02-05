18837bd65cada75de4377fbb14d28cebecc5f15f
arunprop <valluruarunkumar@gmail.com>
2025-02-04 22:04:57 -0600

    -->Updated DB of InventoryService

--- inverntory-service/src/main/java/onlineapplicationInverntoryservice/model/Inventory.java
+++ inverntory-service/src/main/java/onlineapplicationInverntoryservice/model/Inventory.java
@@ -1,22 +1,42 @@
 package onlineapplicationInverntoryservice.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "t_inventory")
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@Builder
 public class Inventory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public String getSkuCode() {
+        return skuCode;
+    }
+
+    public void setSkuCode(String skuCode) {
+        this.skuCode = skuCode;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
     private String skuCode;
 
